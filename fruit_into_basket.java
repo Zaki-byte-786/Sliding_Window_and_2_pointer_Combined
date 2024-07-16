e563156e0212c2d6039841ef476efc7c243bbac7
Zaki-byte-786 <146849204+Zaki-byte-786@users.noreply.github.com>
2024-07-16 11:02:32 +0530

    Add files via upload

--- fruit_into_basket.java
+++ fruit_into_basket.java
@@ -21,12 +21,7 @@ public class fruit_into_basket {
         int l = 0,r = 0,max = 0;
         int n = array.length;
         while (r < n){
-            if (map.containsKey(array[r])){
-                map.put(array[r], map.get(array[r]) + 1);
-            }
-            else {
-                map.put(array[r],1);
-            }
+          map.put(array[r],map.getOrDefault(array[r],0) + 1);
             if (map.size() > 2){
                 while (map.size() != 2){
                     map.put(array[l],map.get(array[l])-1);
@@ -42,15 +37,6 @@ public class fruit_into_basket {
     public static void main(String[] args) {
         int[] fruits  = {3,3,3,1,2,1,1,2,3,3,4};
         System.out.println(fruit_into_basket_Brute(fruits));
-//        HashMap<Integer,Integer> map = new HashMap<>();
-//        map.put(1,2);
-//        map.put(2,3);
-//        map.put(1,map.get(1) - 1);
-//        System.out.println(map);
-//        map.put(2,map.get(2) + 1);
-//        System.out.println(map);
-//        map.remove(1);
-//        System.out.println(map);
         System.out.println(fruit_into_basket_Optimal(fruits));
     }
 }
